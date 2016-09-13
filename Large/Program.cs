037bf8ae680cca38cee6734adcf9147ad258be51
uday.theegalapalle <uday.theegalapalle@INHYDD1024>
2016-09-13 16:51:31 +0530

    Add project files.

---
+++ Large/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Large
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            Console.WriteLine("enter numbers");
+            for (int i = 0; i <arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = arr[0];
+            for (int k= 0; k <arr.Length; ++k)
+            {
+                for (int j = k + 1; j <arr.Length; ++j)
+                {
+                    if (arr[k] < arr[j])
+                    {
+                        int a = arr[k];
+                        arr[k] = arr[j];
+                        arr[j] = a;
+                    }
+                }
+            }
+            for (int m = 0; m < arr.Length; m++)
+            {
+                Console.Write(arr[m]);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
